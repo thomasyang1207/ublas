b10325cbd9cc342425eeffc16ceff93e6fcac22d
Paul Leopardi <leopardi@users.sourceforge.net>
2019-05-26 18:31:30 +1000

    Eigenvalues are returned via vectors

--- doc/samples/eigen_solver.cpp
+++ doc/samples/eigen_solver.cpp
@@ -10,30 +10,35 @@
 #include <boost/numeric/ublas/io.hpp>
 #include <boost/numeric/ublas/eigen_solver.hpp>
 #include <complex>
+#include <sstream>
 
 int main () {
     using namespace boost::numeric::ublas;
+    using std::string;
     matrix<double> m(3, 3);
-    m <<= 2.0, 3.0, 5.0,
-        2.0, -3.0, 7.0,
-        4.0, 1.0, 1.0;
+    static const string m_string =
+        "[3,3](( 2.0,  3.0,  5.0),"
+              "( 2.0, -3.0,  7.0),"
+              "( 4.0,  1.0,  1.0))";
+    std::istringstream is(m_string);
+    is >> m;
     eigen_solver<matrix<double> > es(m,EIGVEC);
 
-    matrix<double> evals_r = es.get_eigenvalues_real();
-    matrix<double> evals_i = es.get_eigenvalues_imag();
+    vector<double> evals_r = es.get_eigenvalues_real();
+    vector<double> evals_i = es.get_eigenvalues_imag();
     matrix<double> evecs_r = es.get_eigenvectors_real();
     matrix<double> evecs_i = es.get_eigenvectors_imag();
 
-    std::cout << "Eigenvalues (Real Part)\n";
+    std::cout << "Eigenvalues (Real Part)" << std::endl;
     std::cout << evals_r << std::endl;
 
-    std::cout << "Eigenvalues (Imag Part)\n";
+    std::cout << "Eigenvalues (Imag Part)" << std::endl;
     std::cout << evals_i << std::endl;
 
-    std::cout << "Eigenvectors (Real Part)\n";
+    std::cout << "Eigenvectors (Real Part)" << std::endl;
     std::cout << evecs_r << std::endl;
 
-    std::cout << "Eigenvectors (Imag Part)\n";
+    std::cout << "Eigenvectors (Imag Part)" << std::endl;
     std::cout << evecs_i << std::endl;
 
     std::cout << "Verification\n";
@@ -42,9 +47,11 @@ int main () {
     matrix<std::complex<double> > A(3,3);
     matrix<std::complex<double> > Lambda;
     for (int i = 0; i < 3; i++){
+        D(i, i) = std::complex<double>(evals_r(i), evals_i(i));
         for (int j = 0; j < 3; j++){
             V(i, j) = std::complex<double>(evecs_r(i, j), evecs_i(i, j));
-            D(i, j) = std::complex<double>(evals_r(i, j), evals_i(i, j));
+            if (i != j)
+                D(i, j) = std::complex<double>(0.0, 0.0);
             A(i, j) = std::complex<double>(m(i, j), 0.0);
         }
     }
